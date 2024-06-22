6ebb01635ba52d22d341d6c0478e26c7ea1f46ce
hiunicorn2024 <hi_unicorn@outlook.com>
2024-06-22 11:05:24 +0800

    Add files via upload

---
+++ clean.bat
@@ -0,0 +1,23 @@
+cd ./doc
+call clean.bat
+cd ../
+
+cd ./immediate_test
+call clean.bat
+cd ../
+
+cd ./immediate_test_no_auto_open
+call clean.bat
+cd ../
+
+cd ./immediate_test_win32
+call clean.bat
+cd ../
+
+cd ./manual_test
+call clean.bat
+cd ../
+
+cd ./auto_test
+call clean.bat
+cd ../
